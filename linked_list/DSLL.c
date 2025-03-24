341de12e6fae46653268e2369df18ecec1a9f5b1
Mkamble3196 <mandarkamble22@gmail.com>
2025-03-24 23:29:45 +0530

    Updated Cleanly end and after algorithm

--- linked_list/DSLL.c
+++ linked_list/DSLL.c
@@ -1,3 +1,4 @@
+/* Double Linked List Implementation*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -11,7 +12,93 @@
 struct node
 {
     int data;
-    struct node* next;
-    struct node* prev;
+    int node* prev;
+    int node* next;
 };
 
+struct node* create_list(void);
+void insert_start(struct node* p_list, int new_data);
+void insert_end(struct node* p_list, int new_data);
+int  insert_after(struct node* p_list, int e_data, int new_data);
+
+struct node* create_list(void)
+{
+    struct node* p_list = NULL;
+
+    p_list = (struct node*)malloc(sizeof(struct node));
+
+    if(p_list == NULL)
+    {
+        puts("Allocation Error:malloc() Failed");
+        exit(EXIT_FAILURE);
+    }
+
+    p_list->data = 0;
+    p_list->next = NULL;
+    p_list->prev = NULL;
+
+    return (p_list);
+}
+
+void insert_start(struct node* p_list, int new_data)
+{
+    struct node* p_new_node = NULL;
+
+    p_new_node = create_list(void);
+
+    p_new_node->data = new_data;
+    p_new_node->prev = p_list;
+    p_new_node->next = p_list->next;
+
+    if(p_list != NULL)
+        p_list->next->prev = p_new_node;
+    
+    p_list->next = p_new_mode; 
+
+}
+
+void insert_end(struct node* p_list, int new_data)
+{
+    struct node* run = NULL;
+
+    run = p_list;
+
+    while(run->next != NULL)
+        run = run->next;
+
+    run->next = create_node();
+
+    run->next->data = new_data;
+    run->next->prev = run;
+    run->next->next = NULL;
+}
+
+int insert_after(struct node* p_list, int e_data, int new_data)
+{
+    struct node* e_node = NULL;
+    struct node* p_new_node = NULL;
+
+    e_node = p_list->next;
+
+    while(e_node != NULL)
+    {
+        if(e_node->data == e_data)
+            break;
+        e_node = e_node->next;
+    }
+
+    if(e_node == NULL)
+        return(LIST_INVALID_DATA);
+
+    p_new_node = create_node();
+
+    p_new_node->data = new_data;
+    p_new_node->next = e_node->next;
+    p_new_node->prev = e_node;
+
+    if(e_node->next != NULL)
+        e_node->next->prev = p_new_node;
+    e_node->next = p_new_node;
+
+    return(SUCCESS);
+}
