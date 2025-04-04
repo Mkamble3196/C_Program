a2bc87530e1730317204bb5521186b68c8761fc6
Mkamble3196 <mandarkamble22@gmail.com>
2025-04-04 11:37:41 +0530

    Reverse Bit logic

---
+++ meshtek/BIT_REVERSE.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i_num = 0;
+    int i_pos = 0;
+    int i = 0;
+    int j = 0;
+    int m ,n ;
+
+    printf("Enter the Number:-");
+    scanf("%d", &i_num);
+
+    printf("Before Reverse:-");
+    for(i_pos = 31; i_pos >= 0 ; i_pos--)
+    {
+        printf("%d", i_num >> i_pos & 1);
+    }
+
+    //Reverse logic
+
+    for(i = 0, j = 31; i < j; i++, j--)
+    {
+        m = i_num >> i & 1;
+        n = i_num >> j & 1; // extract bit 
+
+        if(m != n) // if it is different we need to toggle for reverse order.
+        {
+            i_num = i_num ^ 1 << i;
+            i_num = i_num ^ 1 << j;
+        }
+
+    }
+
+    printf("\n\nAFTER Reverse:-");
+    for(i_pos = 31; i_pos >= 0 ; i_pos--)
+        printf("%d", i_num >> i_pos & 1);
+
+    return(0);
+}
