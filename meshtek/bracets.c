af7a7c54bbf197f8a1ddff81d3218f79d21eec70
Mkamble3196 <mandarkamble22@gmail.com>
2025-04-05 08:17:56 +0530

    interview coding problem and solution

--- meshtek/bracets.c
+++ meshtek/bracets.c
@@ -1,3 +1,37 @@
+/*
+* Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+* An input string is valid if:
+* 
+* Open brackets must be closed by the same type of brackets.
+* Open brackets must be closed in the correct order.
+*  
+* Example 1:
+* Input: s = "()"
+* Output: true
+* 
+* Example 2:
+* Input: s = "()[]{}"
+* Output: true
+* 
+* Example 3:
+* Input: s = "([])"
+* Output: true
+* 
+* Example 4:
+* Input: s = "([)]"
+* Output: false
+* 
+* Example 5:
+* Input: s = "([{)}]"
+* Output: false
+* 
+* Constraints:
+* 
+* 1 <= s.length <= 100
+* s consists of parentheses only '()[]{}'.
+* ==========================================================================
+*/
+
 #include<stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -7,19 +41,20 @@
 int check_string(char* s, char *c_temp)
 {
   int j = -1;
+
   for(int i = 0; s[i] != '\0'; i++)
   {
   	if(s[i] == '(' || s[i] == '{' || s[i] == '[')
     	  c_temp[++j] = s[i];
     else
     {
-        if(j == -1)
-          return 0;
+      if(j == -1)
+        return 0;
           
       if((c_temp[j] == '(' && s[i] == ')') || 
           (c_temp[j] == '[' && s[i] == ']') || 
               (c_temp[j] == '{' && s[i] == '}'))
-        	  j--;
+              j--;
       else
         return 0;
       
@@ -43,9 +78,9 @@ int main(void)
   ret = check_string(s, c_temp);
 
   if(ret == -1)
-    printf("true\n");
+    printf("True\n");
   else
-    printf("false\n");
+    printf("False\n");
     	
   return(0);
 }
