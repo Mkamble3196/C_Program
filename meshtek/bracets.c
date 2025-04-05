7b8c237dce6e64c9718075f2337ae0fe4a0c6bdf
Mkamble3196 <mandarkamble22@gmail.com>
2025-04-05 08:07:47 +0530

    interview coding problem and solution

--- meshtek/bracets.c
+++ meshtek/bracets.c
@@ -2,16 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(void)
+#define MAX   100
+
+int check_string(char* s, char *c_temp)
 {
-	char *s = NULL;
-  char c_temp [100];
   int j = -1;
-  s = (char*)malloc(100 * sizeof(char));
-  
-  printf("Input:s= ");
-  scanf("%s", s);
-  
   for(int i = 0; s[i] != '\0'; i++)
   {
   	if(s[i] == '(' || s[i] == '{' || s[i] == '[')
@@ -20,23 +15,32 @@ int main(void)
       }
     else{
         if(j == -1)
-        { 
-          printf("false_23\n");
           return 0;
-        }
-    
+          
       if((c_temp[j] == '(' && s[i] == ')') || (c_temp[j] == '[' && s[i] == ']') || (c_temp[j] == '{' && s[i] == '}'))
-        {
-    	    j--;
-        }
-      else {
-    	  printf("false\n");
+        	    j--;
+      else
         return 0;
-      }
+      
     }
   }
-  
-  if(j == -1)
+ 
+  return j;
+}
+
+int main(void)
+{
+	char *s = NULL;
+  char *c_temp;
+  int j = -1;
+  int ret = 0;
+  s = (char*)malloc(MAX * sizeof(char));
+  c_temp =(char*)malloc(MAX * sizeof(char));
+  printf("Input:s= ");
+  scanf("%s", s);
+
+  ret = check_string(s, c_temp);
+  if(ret == -1)
 		printf("true\n");
   else
   	printf("false\n");
