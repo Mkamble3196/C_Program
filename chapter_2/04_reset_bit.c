46e6869c9468dc7994b30c509ceaedc7183322f2
Mandar Mahesh Kamble <mandarkamble22@gmail.com>
2025-08-18 23:12:48 +0530

    Resetting corrsponding bit

---
+++ chapter_2/04_reset_bit.c
@@ -0,0 +1,29 @@
+/* 
+Given two number, say val and key. wherever the bits of number key are 1, reset (i.e make 0)
+the corresponding bits of number val. leave all other bits of number val unchanged.
+
+*/
+#include <stdio.h>
+int main(void)
+{
+
+    int val, key, temp;
+    printf("Enter two Number\t");
+    scanf("%d %d",&val, &key);
+    printf("\nBefore\n");
+    for(int i = 31; i >=0 ; i--)
+    {
+        printf("%d", ((val >> i )& 1));
+    }
+    temp = ~key;
+    val = val & temp;
+
+    printf("\nAfter\n");
+    for(int i = 31; i >=0 ; i--)
+    {
+        printf("%d", ((val >> i )& 1));
+    }
+
+    printf("\nafter resetting bits results is %d\n",val);
+    return(0);
+}
