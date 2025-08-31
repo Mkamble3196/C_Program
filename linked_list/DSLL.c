cfe9b7656fac14813a20d6eab2ee084d59a90822
Mandar Mahesh Kamble <mandarkamble22@gmail.com>
2025-08-31 23:59:20 +0530

    Added insert_before function

--- linked_list/DSLL.c
+++ linked_list/DSLL.c
@@ -21,6 +21,7 @@ void insert_start(struct node* p_list, int new_data);
 void insert_end(struct node* p_list, int new_data);
 int  insert_after(struct node* p_list, int e_data, int new_data);
 int remove_start(struct node* p_list);
+int insert_before(struct node* p_list, int e_data, int new_data); 
 
 struct node* create_list(void)
 {
@@ -149,4 +150,37 @@ int remove_start(struct node* p_list)
     first_node = NULL;
 
     return(SUCCESS);
+}
+
+int insert_before(struct node* p_list, int e_data, int new_data)
+{
+    struct node* e_node = NULL;
+    struct node* p_new_node = NULL;
+    e_node = p_list->next; 
+    while(e_node != NULL)
+    {
+        if(e_node->data == e_data)
+            break; 
+        e_node = e_node->next; 
+    }
+
+    if(e_node == NULL)
+        return (LIST_INVALID_DATA); 
+
+    p_new_node = (struct node*)malloc(sizeof(struct node)); 
+    if(p_new_node == NULL)
+    {
+        puts("AllocationError:malloc() failed"); 
+        exit(EXIT_FAILURE); 
+    }
+
+    p_new_node->data = new_data;
+    p_new_node->next = e_node; 
+    p_new_node->prev = e_node->prev; 
+    e_node->prev->next = p_new_node; 
+    e_node->prev = p_new_node; 
+
+    return (SUCCESS); 
+
+
 }
