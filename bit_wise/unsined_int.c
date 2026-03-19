ad54c444296829076d964702159404cd9b5161a7
Mandar Mahesh Kamble <mandarkamble22@gmail.com>
2026-03-19 15:27:45 +0530

    modified

--- bit_wise/unsined_int.c
+++ bit_wise/unsined_int.c
@@ -7,24 +7,13 @@ int main(void)
     unsigned int y = 0;
 
     y = y | (x & 0xf0000000) >> 4;
-     printf("number =0x%X\nafter Reversal = 0x%X\n", x, y); 
     y = y | (x & 0x0f000000) << 4;
-     printf("number =0x%X\nafter Reversal = 0x%X\n", x, y);
-    
     y = y | (x & 0x00f00000) >> 4;
-     printf("number =0x%X\nafter Reversal = 0x%X\n", x, y);
     y = y | (x & 0x000f0000) << 4;
-     printf("number =0x%X\nafter Reversal = 0x%X\n", x, y);
-
     y = y | (x & 0x0000f000) >> 4;
-    printf("number =0x%X\nafter Reversal = 0x%X\n", x, y);
     y = y | (x & 0x00000f00) << 4;
-     printf("number =0x%X\nafter Reversal = 0x%X\n", x, y);
-
     y = y | (x & 0x000000f0) >> 4;
-     printf("number =0x%X\nafter Reversal = 0x%X\n", x, y);
     y = y | (x & 0x0000000f) << 4;
-    printf("number =0x%X\nafter Reversal = 0x%X\n", x, y);
 
     printf("number =0x%X\nafter Reversal = 0x%X\n", x, y);
 
