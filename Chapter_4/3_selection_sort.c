68dac4328c9610dc0df194a93ab93272376cd3b4
Mandar Mahesh Kamble <mandarkamble22@gmail.com>
2026-04-02 14:12:06 +0530

    Deleted uncessary lines

--- Chapter_4/3_selection_sort.c
+++ Chapter_4/3_selection_sort.c
@@ -14,31 +14,20 @@ int main(void)
 	for(i = 0 ; i < NUM ; i++)
 		scanf("%d", &list[i]);
 
-
 	for(i = 0; i < NUM - 1; i++)
 	{
 		min = i;
-	
-		printf("before list[%d] = %d\n",i,list[i]);
 		for(j = i + 1; j < NUM; j++)
 			if(list[j] < list[min])
-			{
 				min = j;
-			printf("\nmin= %d\tj = %d\t list[j=%d]= %d\tlist[m= %d]=%d\n", min, j,j,list[j], min, list[min]);
-			}
-		
 		{
-			printf("\nmin= %d\ti = %d\t", min, i);
 			temp = list[min];
 			list[min] = list[i];
 			list[i] = temp;
 		}	
 	
-		printf("afterlist[%d] =%d\n ",i, list[i]);
 	}
 	
-	printf("\n");
-
 	for(i = 0; i < NUM; i++)
 		printf("%d ",list[i]);
 
